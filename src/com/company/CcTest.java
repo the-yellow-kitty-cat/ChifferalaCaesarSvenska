11711c794339366c12c55e21a64aa08fc6f5b86f
the-yellow-kitty-cat <the.yellow.kitty.cat@gmail.com>
2016-05-19 17:56:00 +0200

    Fler test

--- src/com/company/CcTest.java
+++ src/com/company/CcTest.java
@@ -56,4 +56,16 @@ public class CcTest {
         String testResult = Cc.aTillZ(s);
         assertEquals("defghijklmnopqrstuvwxyzåäöabc", testResult);
     }
+    @org.junit.Test
+    public void testYxskaftbud() throws Exception {
+        String s = "Yxskaftbud, ge vår WC-zonmö IQ-hjälp.";
+        String testResult = Cc.aTillZ(s);
+        assertEquals("Äåvndiwexg, jh yau ZF-örqpc LT-kmbos.", testResult);
+    }
+    @org.junit.Test
+    public void testLynx() throws Exception {
+        String s = "Mr. Jock, TV quiz PhD, bags few lynx.";
+        String testResult = Cc.aTillZ(s);
+        assertEquals("Pu. Mrfn, WY txlö SkG, edjv ihz oäqå.", testResult);
+    }
 }
